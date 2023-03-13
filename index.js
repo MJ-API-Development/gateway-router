d206ff320a548970082348c84bc9789c4c22d820
freelancing-solutions <mobiusndou@gmail.com>
2023-03-13 12:20:24 +0200

    Included Request Headers to show the request comes from cloudflare

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 export default {
   async fetch(request, env) {
     try {
-      const { pathname } = new URL(request.url);
+      let { pathname } = new URL(request.url);
       // Proxy requests through Cloudflare for certain paths
-      if (pathname.startsWith("/api") || pathname.startsWith("/_admin") || pathname.startsWith("/open-api")) {
-        // Create a new request that shows it is coming from Cloudflare
-        const cfRequest = new Request(request);
-        cfRequest.headers.set("CF-Connecting-IP", request.headers.get("CF-Connecting-IP"));
-        // Fetch the resource from the origin server
-        // Return the response to the client
-        return await fetch(cfRequest);
+      const allowedPaths = ["/api", "/redoc", "/docs", "/_admin", "/open-api"];
+      const homeRoute = "/";
+
+      if (pathname === homeRoute){
+        pathname = "/redoc";
+      }
+
+      if (allowedPaths.some(prefix => pathname.startsWith(prefix))) {
+        return await fetch(request)
       }
       // Return a 404 Not Found response for other paths
-      const message_err = {'message': 'URL not Found'}
+      const message_err = {'message': 'URL not Found'};
       return new Response(JSON.stringify(message_err), { status: 404 })
     } catch(e) {
       // Return a 500 Internal Server Error response for errors
